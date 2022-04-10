0132979779c567fd5946740da0454ebd3ca800e4
Ivan Guanipa <ivanguanipa@gmail.com>
2022-04-10 12:36:49 -0400

    code

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { homeRouter as HomeRouter } from 'app/feature/Home/homeRouter';
-import MainPage from 'app/main';
+import { homeRouter as HomeRouter } from 'app/feature/Home/HomeRouter';
+import MainPage from 'app/Main';
 import { NavigationHeader } from 'app/shared/components/NavigationHeader';
 import { PasaporteRouter } from 'app/feature/Pasaporte/PasaporteRouter';
 
